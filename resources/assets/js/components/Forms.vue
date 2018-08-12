3e6079751b54a0e29baaf4983233722c4f57bffa
thukuwanjiku <mikedegr809@gmail.com>
2018-08-12 08:32:07 +0300

    vee-validate

--- resources/assets/js/components/Forms.vue
+++ resources/assets/js/components/Forms.vue
@@ -7,7 +7,7 @@
                     <label class="label">Email</label>
                     <p class="control has-icon has-icon-right">
                         <input name="email" v-model="email" v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('email') }" type="text" placeholder="Email">
-                        <i v-show="errors.has('email')" class="fa fa-warning"></i>
+                        <!-- <i v-show="errors.has('email')" class="fa fa-warning"></i> -->
                         <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
                     </p>
                 </div>
@@ -15,7 +15,7 @@
                     <label class="label">Name</label>
                     <p class="control has-icon has-icon-right">
                         <input name="name" v-model="name" v-validate="'required|alpha'" :class="{'input': true, 'is-danger': errors.has('name') }" type="text" placeholder="Name">
-                        <i v-show="errors.has('name')" class="fa fa-warning"></i>
+                        <!-- <i v-show="errors.has('name')" class="fa fa-warning"></i> -->
                         <span v-show="errors.has('name')" class="help is-danger">{{ errors.first('name') }}</span>
                     </p>
                 </div>
@@ -26,7 +26,7 @@
                     <label class="label">Phone</label>
                     <p class="control has-icon has-icon-right">
                         <input name="phone" v-model="phone" v-validate="'required|numeric'" :class="{'input': true, 'is-danger': errors.has('phone') }" type="text" placeholder="Phone">
-                        <i v-show="errors.has('phone')" class="fa fa-warning"></i>
+                        <!-- <i v-show="errors.has('phone')" class="fa fa-warning"></i> -->
                         <span v-show="errors.has('phone')" class="help is-danger">{{ errors.first('phone') }}</span>
                     </p>
                 </div>
@@ -34,7 +34,7 @@
                     <label class="label">Website</label>
                     <p class="control has-icon has-icon-right">
                         <input name="url" v-model="url" v-validate="'required|url'" :class="{'input': true, 'is-danger': errors.has('url') }" type="text" placeholder="Website">
-                        <i v-show="errors.has('url')" class="fa fa-warning"></i>
+                        <!-- <i v-show="errors.has('url')" class="fa fa-warning"></i> -->
                         <span v-show="errors.has('url')" class="help is-danger">{{ errors.first('url') }}</span>
                     </p>
                 </div>
@@ -52,10 +52,11 @@
 
 <script>
 
-import 'bulma/css/bulma.css'
 import Vue from 'vue';
 import VeeValidate from 'vee-validate';
 Vue.use(VeeValidate);
+
+import 'bulma/css/bulma.css'
     
     export default{
         name:'Forms',
@@ -70,15 +71,10 @@ Vue.use(VeeValidate);
         },
         methods:{
             validateBeforeSubmit() {
-                this.$validator.validateAll().then((result) => {
-                    if (result) {
-                    // eslint-disable-next-line
-                    alert('Form Submitted!');
-                    return;
-                    }
-
-                    alert('Correct them errors!');
-                });
+                if(!this.errors.any()){
+                    alert('Good to go');
+                }
+                alert('Good to go');
             }
         }
     }
